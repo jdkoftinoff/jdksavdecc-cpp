d4010d65d6ba5e9daeb66bd0497976254d9c0d7b
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2013-02-14 13:26:38 -0800

    initial commit

---
+++ project.mk
@@ -0,0 +1,17 @@
+PROJECT=jdksavdecc-cpp
+PROJECT_NAME=jdksavdecc-cpp
+PROJECT_VERSION?=20130106
+PROJECT_EMAIL=jeffk@jdkoftinoff.com
+PROJECT_LICENSE=ISC
+PROJECT_MAINTAINER=jeffk@jdkoftinoff.com
+PROJECT_COPYRIGHT=Copyright 2013
+PROJECT_DESCRIPTION=
+PROJECT_WEBSITE=https://jdksavdecc-cpp.statusbar.com/
+PROJECT_IDENTIFIER=org.statusbar.jdksavdecc-cpp
+TOP_LIB_DIRS+=.
+CONFIG_TOOLS+=
+PKGCONFIG_PACKAGES+=
+TEST_OUT_SUFFIX=txt
+
+CONFIG_TOOLS=
+
